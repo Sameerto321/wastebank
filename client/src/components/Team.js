23d2769a4e7555e2030c371f61587fb4fe47fa2f
Sameer Hussain <87130217+Sameer0018@users.noreply.github.com>
2021-11-11 16:28:57 +0530

    first commit

---
+++ client/src/components/Team.js
@@ -0,0 +1,247 @@
+import React from 'react';
+import  '../css/Team.css';
+
+const Team = () => {
+  
+    return(
+    <>
+    
+    <div className="team-area">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12">
+        <div className="section-title text-center">
+          <h2>Our Team</h2>
+          <p>Meet Our telented Team Members</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="section-title">
+            <h2>
+              <b>Founder/Creator - </b>
+            </h2>
+          </div>
+        </div>
+        {/* {/== Single Team Item ==/} */}
+        <div className="row">
+          <div className="col-md-3">
+            <div className="single-team">
+              <div className="team-img">
+                <img src="Images/Shubham_Khandelwal.jpeg" alt />
+              </div>
+              <div className="team-content">
+                <div className="team-info">
+                  <h3>Shubham Khandelwal</h3>
+                  {/* <p>Project Manager</p> */}
+                  <br />
+                  <br />
+                </div>
+                <p className="team-text">
+                  A reverse engineering enthusiast, nature lover. Creator of
+                  flub waste bank.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="row">
+          <div className="col-md-12">
+            <div className="section-title">
+              <h2>
+                <b>Interns - </b>
+              </h2>
+            </div>
+          </div>
+          {/* {/== Single Team Item ==/} */}
+          <div className="col-md-3">
+            <div className="single-team">
+              <div className="team-img">
+                <img
+                  src="Images/Aditi_Tomar.jpeg"
+                  alt
+                  className="img-responsive"
+                />
+              </div>
+              <div className="team-content">
+                <div className="team-info">
+                  <h3>Aditi Tomar</h3>
+                  {/* <p>Intern</p> */}
+                  <br />
+                  <br />
+                </div>
+                <p className="team-text">
+                  Passionate about technology and love to network with like
+                  minded professionals. The key projects which I worked on
+                  during my internship included usage of languages like HTML,
+                  CSS, Javascript, NodeJs and Express Js. I also started getting
+                  interested more in Web development and basics of Python
+                  language. I also know C and C++.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* {/== Single Team Item ==/} */}
+          <div className="col-md-3">
+            <div className="single-team">
+              <div className="team-img">
+                <img
+                  src="Images/Alpit_Namdev.jpeg"
+                  alt
+                  className="img-responsive"
+                />
+              </div>
+              <div className="team-content">
+                <div className="team-info">
+                  <h3>Alpit Namdev</h3>
+                  {/* <p>Intern</p> */}
+                  <br />
+                  <br />
+                </div>
+                <p className="team-text">N/A</p>
+              </div>
+            </div>
+          </div>
+          {/* {/== Single Team Item ==/} */}
+          <div className="col-md-3">
+            <div className="single-team">
+              <div className="team-img">
+                <img
+                  src="Images/Diksha_Sharma.jpeg"
+                  alt
+                  className="img-responsive"
+                />
+              </div>
+              <div className="team-content">
+                <div className="team-info">
+                  <h3>Diksha Sharma</h3>
+                  {/* <p>Intern</p> */}
+                  <br />
+                  <br />
+                </div>
+                <p className="team-text">
+                  A tech-enthusiast, Worked on several projects as frontend developer. Comfortable
+                  in working with React JS, Javascript, HTML and CSS. I also have 
+                  exposure to MERN stack & good knowledge of C/C++
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="single-team">
+              <div className="team-img">
+                <img
+                  src="Images/Harsha_Sharma_3.jpeg"
+                  alt
+                  className="img-responsive"
+                />
+              </div>
+              <div className="team-content">
+                <div className="team-info">
+                  <h3>Harsha Sharma</h3>
+                  {/* <p>Intern</p> */}
+                  <br />
+                  <br />
+                </div>
+                <p className="team-text">
+                  A passionate developer specializing mostly in PHP and
+                  javascript. Fluent in HTML,CSS and Bootstrap. Skilled in
+                  various programming languages such as C,C++ and also have a
+                  good knowledge of data structures. Have managed to work on
+                  quite a few development projects.{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <br /> {/<br>/} */}
+        <br/>
+        <div className="row">
+          <div className="col-md-3">
+            <div className="single-team">
+              <div className="team-img">
+                <img src="Images/Rashika_Sharma.jpeg" alt />
+              </div>
+              <div className="team-content">
+                <div className="team-info">
+                  <h3>Rashika Sharma</h3>
+                  {/* <p>Intern</p> */}
+                  <br />
+                  <br />
+                </div>
+                <p className="team-text">
+                  A computer science enthusiast. I recently completed my python,
+                  Django &amp; IOT training.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* {/== Single Team Item ==/} */}
+          <div className="col-md-3">
+            <div className="single-team">
+              <div className="team-img">
+                <img
+                  src="Images/Sameer_Hussain.jpeg"
+                  alt
+                  className="img-responsive"
+                />
+              </div>
+              <div className="team-content">
+                <div className="team-info">
+                  <h3>Sameer Hussain</h3>
+                  {/* <p>Intern</p> */}
+                  <br />
+                  <br />
+                </div>
+                <p className="team-text">
+                  A level-headed person, organizing everything to work
+                  efficiently and achieving the goals that I set.I am calm and
+                  think quickly in stressful situations,I want to change the
+                  world but God won't give me the source code.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* {/== Single Team Item ==/} */}
+          <div className="col-md-3">
+            <div className="single-team">
+              <div className="team-img">
+                <img
+                  src="Images/Suraj_Sharma.jpeg"
+                  alt
+                  className="img-responsive"
+                />
+              </div>
+              <div className="team-content">
+                <div className="team-info">
+                  <h3>Suraj Sharma</h3>
+                  {/* <p>Intern</p> */}
+                  <br />
+                  <br />
+                </div>
+                <p className="team-text">
+                  I am passionate about my work. Because I love what I do, I
+                  have a steady source of motivation that drives me to do my
+                  best.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+	
+    </>
+  );
+  }
+  
+  export default Team;
